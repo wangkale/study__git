423e0e8bec613ab2daf5e7b923a1a632fa60b0c4
wangkale <1464042168@qq.com>
2021-07-12 16:48:48 +0800

    aaaaaaaaaaa

--- 8.c
+++ 8.c
@@ -5,7 +5,7 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
-#include<errno.h>
+#in                :clude<errno.h>
 #include<string.h>
 #include<fcntl.h>
 int main(int argc, char* argv[])
